5de41e9cd3ce67d493e2690cf0de2a773cb9ff37
MartinCervenka00 <martincervenka1@gmail.com>
2026-04-11 16:16:18 +0100

    ni outline colour and width
    changing the colour of ni outline and the width of that line is 1. no face colour.

--- ni_collision_analysis.py
+++ ni_collision_analysis.py
@@ -10,6 +10,9 @@ outline = gpd.read_file("data/NI_outline.shp")
 
 print("Data loaded")
 
-outline.plot()
+# Changing map to black line with width 1 with no face colour
+fig, ax = plt.subplots()
+outline.plot(ax=ax, facecolor="none", edgecolor="black", linewidth=1)
+
 plt.title("Northern Ireland Outline")
 plt.show()
