00690b7ef5992464cafbce598d00638ec21a769c
MartinCervenka00 <martincervenka1@gmail.com>
2026-04-11 16:05:53 +0100

    load collision data
    Adding collision data as csv and ni outline

---
+++ ni_collision_analysis.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import geopandas as gpd
+import matplotlib.pyplot as plt
+
+# Load collision data
+collisions = pd.read_csv("data/collisions_2024.csv")
+
+# Load NI outline
+outline = gpd.read_file("data/NI_outline.shp")
+
+print("Data loaded")
