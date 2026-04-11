edc831d9f9e6c6aaf055c3a0fff8c4b37ae5fd68
MartinCervenka00 <martincervenka1@gmail.com>
2026-04-11 17:45:05 +0100

    creating collision points
    Match crs to collision data. Area is being plotted for both ni outline and collision

--- ni_collision_analysis.py
+++ ni_collision_analysis.py
@@ -14,5 +14,22 @@ print("Data loaded")
 fig, ax = plt.subplots()
 outline.plot(ax=ax, facecolor="none", edgecolor="black", linewidth=1)
 
-plt.title("Northern Ireland Outline")
+
+# Creating collision points (TM65 Irish Grid - EPSG = 29901, where a_gd1 = Easting and a_gd2 = Northing)
+collisions_gdf = gpd.GeoDataFrame(
+    collisions,
+    geometry=gpd.points_from_xy(collisions["a_gd1"], collisions["a_gd2"]),
+    crs="EPSG:29901"
+)
+
+# Match crs to collision data
+outline = outline.to_crs(epsg=29901)
+
+# Area is being plotted for both ni outline and collision
+fig, ax = plt.subplots()
+outline.plot(ax=ax, color="white", edgecolor="black")
+collisions_gdf.plot(ax=ax, color="red", markersize=1)
+
+
+plt.title("Road traffic collisions in Northern Ireland (2024)")
 plt.show()
